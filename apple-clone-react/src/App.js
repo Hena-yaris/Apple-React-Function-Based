15d2e8caf25e86397c4dd02c3268970a26ad22a8
Hena-yaris <meraey21@gmail.com>
2024-02-12 15:15:21 -0800

    add bootstarp ,custome css,jquery and include developer name additional including Routes

--- apple-clone-react/src/App.js
+++ apple-clone-react/src/App.js
@@ -1,9 +1,50 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import React from "react";
+import "./bootstrap.css";
+import "./Style.css";
+
+
+//Home Page
+import Nav from "./Components/Nav/Nav";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Home/Home";
+
+
+
+// Pages
+import Iphone from "./Components/Pages/Iphone/iphone";
+import Mac from "./Components/Pages/Mac/Mac";
+import Ipad from "./Components/Pages/Ipad/Ipad";
+import Music from "./Components/Pages/Music/Music";
+import Cart from "./Components/Pages/Cart/Cart";
+import Support from "./Components/Pages/Support/Support";
+import TV from "./Components/Pages/TV/TV";
+import Watch from "./Components/Pages/Watch/Watch";
+import Four04 from "./Components/Pages/Four04/Four04";
+import Productpage from "./Components/Pages/Productpage/Productpage";
+
 
 function App() {
   return (
     <div className="App">
-     
+      <Nav />
+
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/iphone" element={<Iphone/>} />
+        <Route path="/ipad" element={<Ipad/>} />
+        <Route path="/mac" element={<Mac/>} />
+        <Route path="/music" element={<Music/>} />
+        <Route path="/productpage" element={<Productpage/>} />
+        <Route path="/support" element={<Support/>} />
+        <Route path="/tv" element={<TV/>} />
+        <Route path="/watch" element={<Watch/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path='*' element={<Four04/>}/>
+      </Routes>
+
+      <Footer />
     </div>
   );
 }
---
+++ apple-clone-react/src/Home/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+//Home page
+import  Paragraph from "../Components/Paragraph/Paragraph"
+import First from "../Components/First/First"
+import Second from "../Components/Second/Second"
+import Third from "../Components/Third/Third"
+import Fourth from "../Components/Fourth/Fourth"
+import Fifth from "../Components/Fifth/Fifth"
+import Sixth from "../Components/Sixth/Sixth"
+
+ function Home() {
+  return (
+    <>
+      <Paragraph/>
+      <First/>
+      <Second/>
+      <Third/>
+      <Fourth/>
+      <Fifth/>
+      <Sixth/>
+    </>
+  )
+}
+
+export default Home;
